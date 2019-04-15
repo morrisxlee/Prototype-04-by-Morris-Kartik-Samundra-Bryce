4bb3db672b8e5cdaa8bd671b9c80cea9b0917c53
Morris <morrisxlee@gmail.com>
2019-04-15 11:18:19 +1000

    Added Start button
    Added Start button

---
+++ Prototype 04 Unity Project/Assets/Script/ChangeScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public void changeToScene(int changeTheScene)
+    {
+        SceneManager.LoadScene(changeTheScene);
+    }
+
+    //This code is for quit the game
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
